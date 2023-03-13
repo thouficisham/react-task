c0cd0f754425ac1ac8bbb818fb78710b934071f1
thoufic-isham <mohammedthouficishamas@gmail.com>
2023-03-13 12:14:19 +0530

    footer-fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css';
-import Layout from "./components/Layout"
-import nipponlogo from "../src/assets/download.webp"
+import "./App.css";
+import Layout from "./components/Layout";
+import nipponlogo from "../src/assets/download.webp";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nipponlogo} alt='nippon logo' style={{positiob: 'relative', width: '50px', height: '50px', right: '100px'}}/>
+        <img
+          src={nipponlogo}
+          alt="nippon logo"
+          style={{
+            positiob: "relative",
+            width: "50px",
+            height: "50px",
+            right: "100px",
+          }}
+        />
         Header
       </header>
-      <Layout />
-      <footer className="App-footer">
-        Footer
-      </footer>
+      <div>
+        <Layout />
+      </div>
+      <footer className="App-footer">Footer</footer>
     </div>
   );
 }
